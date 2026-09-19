aca06505714b6daded59c0778bc718d27799def0
unknown <andreanrossy32@gmail.com>
2026-09-19 23:32:31 +0700

    tampilan dashboard, kelola user untuk admin

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengukuranController;
 
 Route::get('/', function () {
-    return view('stuntguard');
+    return view('admin.dashboardAdmin');
+});
+
+Route::get('/kelolaUser', function () {
+    return view('admin.kelolaUser');
 });
 
 Route::post('/simpan-pengukuran', [PengukuranController::class, 'store']);

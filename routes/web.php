ba4d34afa835da5dd7eb1c240d989d75be5932fc
awndreyy <andreanrossy903@gmail.com>
2026-09-25 23:04:51 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,11 @@ use App\Http\Controllers\PengukuranController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('admin.dashboardAdmin');
-});
+Route::get('/', function () {return view('admin.dashboardAdmin');});
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+Route::get('/login', function () {return view('auth.login');})->name('login');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+Route::get('/register', function () {return view('auth.register');})->name('register');
 
 Route::get('/kelolaUser', [UserController::class, 'index'])->name('users.index');
 
